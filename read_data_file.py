7514d0ee0c75854bc0f15d6316489d819de37959
Aga Nowak <a.nowak@tudelft.nl>
2023-09-12 15:34:31 +0200

    save to csv

--- read_data_file.py
+++ read_data_file.py
@@ -2,6 +2,39 @@
 import sys
 from matplotlib import pyplot as plt
 import numpy as np
+import csv
+
+
+def simulate_yaw_rate(filename):
+    yaw_rates = []
+
+    with open(filename, 'r') as f:
+        yaw_rate = 0.0
+
+        while True:
+            line = f.readline()
+
+            if not line:
+                break
+
+            line = line.strip().split(', ')
+            
+            # (f'{sys_time}, {self.time}, {self._data["stateEstimate.x"]}, {self._data["stateEstimate.y"]}, {self._data["stateEstimate.z"]}, {self._data["jevois.errorx"]}, {self._data["jevois.errory"]}, {self._data["jevois.width"]}, {self._data["jevois.height"]}\n')
+
+            alpha_yaw = 0.8
+            error_y_gain = 0.25
+
+            error_y = float(line[6])
+
+            yaw_rate = alpha_yaw * yaw_rate - (1 - alpha_yaw) * error_y_gain * error_y + 8
+
+            yaw_rates.append(yaw_rate)
+
+    with open('yaw_sim.csv', 'w') as y:
+        wr = csv.writer(y)
+        for yaw_rate in yaw_rates:
+            wr.writerow([yaw_rate])
+
 
 def read_data(filename):
 
@@ -39,16 +72,19 @@ if __name__ == '__main__':
     width = data[:,7]
     height = data[:,8]
 
+    if len(data[0]) <= 9:
+        simulate_yaw_rate(f'data_{n_file}.txt')
+
     # live plot:
-    figure_handle = plt.figure('Live data')
-    plt.ion()
-    for i in range(len(time)):
-        plt.clf()
-        plt.plot(time[:i], error_x[:i], label='error_x')
-        plt.plot(time[:i], error_y[:i], label='error_y')
-        plt.legend()
-        plt.pause(0.01)
-        plt.draw()
+    # figure_handle = plt.figure('Live data')
+    # plt.ion()
+    # for i in range(len(time)):
+    #     plt.clf()
+    #     plt.plot(time[:i], error_x[:i], label='error_x')
+    #     plt.plot(time[:i], error_y[:i], label='error_y')
+    #     plt.legend()
+    #     plt.pause(0.01)
+    #     plt.draw()
 
 
     plt.figure()
