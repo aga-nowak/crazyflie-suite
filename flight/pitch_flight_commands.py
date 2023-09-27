abee64db28f36a3b37e2e13a5b2874aa8351a100
Aga Nowak <a.nowak@tudelft.nl>
2023-09-27 17:19:20 +0200

    call commander correctly

--- flight/pitch_flight_commands.py
+++ flight/pitch_flight_commands.py
@@ -10,7 +10,7 @@ def takeoff(cf, z_distance=1.0):
             cf._cf.extpos.send_extpos(
                 cf.filtered_pos[0], cf.filtered_pos[1], cf.filtered_pos[2]
             )
-        cf.commander.send_zdistance_setpoint(0.0, 0.0, 0.0, z_distance)  # (roll, pitch, yaw rate, z distance)
+        cf._cf.commander.send_zdistance_setpoint(0.0, 0.0, 0.0, z_distance)  # (roll, pitch, yaw rate, z distance)
         time.sleep(0.05)
         time_passed += 0.05
 
@@ -45,5 +45,7 @@ def landing(cf):
             cf._cf.commander.send_zdistance_setpoint(0.0, 0.0, 0.0, 0.0)
             time.sleep(0.05)
             time_passed += 0.05
+
+    cf._cf.commander.send_stop_setpoint()
 
     print('Landed')
--- flightcfg.sh
+++ flightcfg.sh
@@ -14,7 +14,7 @@ python flight/log_flight.py \
     --space flight/space_cyberzoo.yaml \
     --estimator kalman \
     --uwb none \
-    --trajectory none \
+    --trajectory pitch \
     --optitrack none \
     --optitrack_id 1 \
     --uri radio://0/80/2M/E7E7E7E7E7
