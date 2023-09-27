dab64e1527d9c5fe4e1a16e47537172cb66f0c2f
Aga Nowak <a.nowak@tudelft.nl>
2023-09-27 10:02:07 +0200

    Merge branch 'main' of https://github.com/aga-nowak/crazyflie-suite-jevois into main

--- IMAV.py
+++ IMAV.py
@@ -3,18 +3,18 @@ import time
 
 # for logging data:
 # import the sys library
-import sys
+# import sys
 # import the os library
-import os
+# import os
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.utils import uri_helper
-from cflib.crazyflie.log import LogConfig
+# from cflib.crazyflie.log import LogConfig
 
 # for showing live data:
-from IPython import display
-from matplotlib import pyplot as plt
+# from IPython import display
+# from matplotlib import pyplot as plt
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/FDE7E7E701')
 DEFAULT_HEIGHT = 1.0
@@ -26,39 +26,40 @@ logging.basicConfig(level=logging.ERROR)
 
 class gateIMAV:
      
-    def __init__(self, crazyflie,logconf):
+    def __init__(self, crazyflie):
         self._cf = crazyflie
-        self._data = {}
-        self._logconfig = logconf
+        # self._data = {}
+        # self._logconfig = logconf
         self.time = 0
-        self.figure_handle = []
+        # self.figure_handle = []
 
         self.yaw_rate = 0.0
         self.z_distance = DEFAULT_HEIGHT
 
         # Get the files in the current directory:
-        files = os.listdir('.')
+        # files = os.listdir('.')
         # Find how many data files there are currently:
-        data_files = [f for f in files if f.startswith('data')]
-        n_data_files = len(data_files)
+        # data_files = [f for f in files if f.startswith('data')]
+        # n_data_files = len(data_files)
 
         # open a file for writing the data:
-        self._pylogfile = open(f'data_{n_data_files+1}.txt', 'w')
+        # self._pylogfile = open(f'data_{n_data_files+1}.txt', 'w')
 
 
-    def _log_cb(self, timestamp, data, logconf):
-        for key, value in data.items():
-            self._data[key] = value
-        self.time = timestamp/1000
+    # def _log_cb(self, timestamp, data, logconf):
+    #     for key, value in data.items():
+    #         self._data[key] = value
+    #     self.time = timestamp/1000
 
-    def log_data(self):
-        # get the time in ms from the time package:
-        sys_time = time.time()
+    # def log_data(self):
+    #     # get the time in ms from the time package:
+    #     sys_time = time.time()
 
-        # write the data to the file:
-        self._pylogfile.write(f'{sys_time}, {self.time}, {self._data["stateEstimate.x"]}, {self._data["stateEstimate.y"]}, {self._data["stateEstimate.z"]}, {self._data["jevois.errorx"]}, {self._data["jevois.errory"]}, {self._data["jevois.width"]}, {self._data["jevois.height"]}, {self.yaw_rate}, {self.z_distance}\n')
+    #     # write the data to the file:
+    #     self._pylogfile.write(f'{sys_time}, {self.time}, {self._data["stateEstimate.x"]}, {self._data["stateEstimate.y"]}, {self._data["stateEstimate.z"]}, {self._data["jevois.errorx"]}, {self._data["jevois.errory"]}, {self._data["jevois.width"]}, {self._data["jevois.height"]}, {self.yaw_rate}, {self.z_distance}\n')
 
     def takeoff(self):
+        print("takeoff")
         time_passed = 0.0
         while time_passed < 5:
             # we now send the taking off position (x,y,z,yaw)
@@ -69,104 +70,106 @@ class gateIMAV:
             time_passed += 0.05
 
 
-    def fly_through_gate(self, time_limit):
-        print('Window detection')
+    # def fly_through_gate(self, time_limit):
+    #     print('Window detection')
 
-        yaw_rate = 0.0
-        self.z_distance = DEFAULT_HEIGHT
-        old_z = DEFAULT_HEIGHT
+    #     yaw_rate = 0.0
+    #     self.z_distance = DEFAULT_HEIGHT
+    #     old_z = DEFAULT_HEIGHT
 
-        width = self._data['jevois.width']
-        height = self._data['jevois.height']
+    #     width = self._data['jevois.width']
+    #     height = self._data['jevois.height']
 
-        if(SHOW_DATA_LIVE):
-            times = []
-            error_ys = []
-            error_xs = []
-            widths = []
-            heights = []
+    #     if(SHOW_DATA_LIVE):
+    #         times = []
+    #         error_ys = []
+    #         error_xs = []
+    #         widths = []
+    #         heights = []
 
-        time_passed = 0.0
-        while time_passed < time_limit and max(width, height) < 180:
-            # get z and jevois errors
-            z = self._data['stateEstimate.z']
-            error_x = self._data['jevois.errorx']
-            error_y = self._data['jevois.errory']
-            width = self._data['jevois.width']
-            height = self._data['jevois.height']
+    #     time_passed = 0.0
+    #     while time_passed < time_limit and max(width, height) < 180:
+    #         # get z and jevois errors
+    #         z = self._data['stateEstimate.z']
+    #         error_x = self._data['jevois.errorx']
+    #         error_y = self._data['jevois.errory']
+    #         width = self._data['jevois.width']
+    #         height = self._data['jevois.height']
 
-            if abs(old_z - z) > 0.5:
-                z = old_z
-            old_z = z
+    #         if abs(old_z - z) > 0.5:
+    #             z = old_z
+    #         old_z = z
 
-            if(SHOW_DATA_LIVE):
-                # add the data to the lists for live plotting
-                times.append(self.time)
-                error_ys.append(error_y)
-                error_xs.append(error_x)
-                widths.append(width)
-                heights.append(height)
+    #         if(SHOW_DATA_LIVE):
+    #             # add the data to the lists for live plotting
+    #             times.append(self.time)
+    #             error_ys.append(error_y)
+    #             error_xs.append(error_x)
+    #             widths.append(width)
+    #             heights.append(height)
 
-            # log the data for post-analysis
-            self.log_data()
+    #         # log the data for post-analysis
+    #         self.log_data()
 
-            # Computing yawrate and zdistance for the commander (filtered)
-            alpha_yaw = 0.8
-            # error_y_gain = 0.5
-            error_y_gain = 0.25
-            alpha_z = 0.8
-            error_x_gain = 0.001
+    #         # Computing yawrate and zdistance for the commander (filtered)
+    #         alpha_yaw = 0.8
+    #         # error_y_gain = 0.5
+    #         error_y_gain = 0.5
+    #         alpha_z = 0.8
+    #         error_x_gain = 0.001
 
-            self.yaw_rate = alpha_yaw * self.yaw_rate - (1 - alpha_yaw) * error_y_gain * error_y + 8
-            self.z_distance = alpha_z * self.z_distance + (1 - alpha_z) * (error_x_gain * error_x + z)
+    #         self.yaw_rate = alpha_yaw * self.yaw_rate - (1 - alpha_yaw) * error_y_gain * error_y
+    #         self.z_distance = alpha_z * self.z_distance + (1 - alpha_z) * (error_x_gain * error_x + z)
 
-            if self.z_distance > 1.5:
-                self.z_distance = 1.5
+    #         if self.z_distance > 1.5:
+    #             self.z_distance = 1.5
 
-            # print(f'error_y = {error_y}, error_y_gain = {error_y_gain}, alpha_yaw*self.yaw_rate={alpha_yaw*self.yaw_rate}, error_y_gain*error_y={error_y_gain*error_y}, self.yaw_rate={self.yaw_rate}')
-            print(f'errorx = {error_x}, errory = {error_y}, width = {width}, height = {height}')
+    #         # print(f'error_y = {error_y}, error_y_gain = {error_y_gain}, alpha_yaw*self.yaw_rate={alpha_yaw*self.yaw_rate}, error_y_gain*error_y={error_y_gain*error_y}, self.yaw_rate={self.yaw_rate}')
+    #         print(f'errorx = {error_x}, errory = {error_y}, width = {width}, height = {height}')
 
-            # Commanding roll, pitch, yaw rate and z position
-            self._cf.commander.send_zdistance_setpoint(0.0, -10.0, self.yaw_rate, self.z_distance)
+    #         # Commanding roll, pitch, yaw rate and z position
+    #         self._cf.commander.send_zdistance_setpoint(0.0, -10.0, self.yaw_rate, self.z_distance)
 
-            # self._cf.commander.send_setpoint(30.0, 0.0, 0.0, 40000)
-            # self._cf.commander.send_position_setpoint(0.0, 30.0, DEFAULT_HEIGHT, 0.0)
+    #         # self._cf.commander.send_setpoint(30.0, 0.0, 0.0, 40000)
+    #         # self._cf.commander.send_position_setpoint(0.0, 30.0, DEFAULT_HEIGHT, 0.0)
 
-            if(SHOW_DATA_LIVE):
-                if(self.figure_handle == []):
-                    self.figure_handle = plt.figure('Live data')
-                    self.ax = self.figure_handle.add_subplot(111)
-                    plt.ion()
-                    #self.figure_handle.show()
-                    self.figure_handle.canvas.draw()
-                else:
-                    plt.figure('Live data')
+    #         if(SHOW_DATA_LIVE):
+    #             if(self.figure_handle == []):
+    #                 self.figure_handle = plt.figure('Live data')
+    #                 self.ax = self.figure_handle.add_subplot(111)
+    #                 plt.ion()
+    #                 #self.figure_handle.show()
+    #                 self.figure_handle.canvas.draw()
+    #             else:
+    #                 plt.figure('Live data')
                 
-                self.ax.clear()
-                self.ax.plot(times, error_xs, label='error_x')
-                self.ax.plot(times, error_ys, label='error_y')
-                self.ax.plot(times, widths, label='width')
-                self.ax.plot(times, heights, label='height')
-                self.ax.legend()
-                #        self.figure_handle.canvas.draw()
-                #        self.figure_handle.show()
-                display.clear_output(wait=True)
-                display.display(plt.gcf())
+    #             self.ax.clear()
+    #             self.ax.plot(times, error_xs, label='error_x')
+    #             self.ax.plot(times, error_ys, label='error_y')
+    #             self.ax.plot(times, widths, label='width')
+    #             self.ax.plot(times, heights, label='height')
+    #             self.ax.legend()
+    #             #        self.figure_handle.canvas.draw()
+    #             #        self.figure_handle.show()
+    #             display.clear_output(wait=True)
+    #             display.display(plt.gcf())
 
-            time.sleep(0.05)
-            time_passed += 0.05
+    #         time.sleep(0.05)
+    #         time_passed += 0.05
 
-        print('Window passed')
+    #     print('Window passed')
 
 
     def fly_forward(self, time_limit):
-        z = self._data['stateEstimate.z']
+        print('fly forward')
+        # z = self._data['stateEstimate.z']
         time_passed = 0.0
         while time_passed < time_limit:
             # Commanding roll, pitch, yaw rate and z position
-            self._cf.commander.send_zdistance_setpoint(0.0, -10, 0.0, z)
+            # self._cf.commander.send_zdistance_setpoint(0.0, -10, 0.0, z)
+            self._cf.commander.send_zdistance_setpoint(0.0, -10, 0.0, DEFAULT_HEIGHT)
             # log the data:
-            self.log_data()
+            # self.log_data()
 
             time.sleep(0.05)
             time_passed += 0.05
@@ -174,31 +177,31 @@ class gateIMAV:
     
     def landing(self):
         time_passed = 0.0
-        while time_passed < 10 and self._data['stateEstimate.z'] > 0.1:
-            x = self._data['stateEstimate.x']
-            y = self._data['stateEstimate.y']
+        while time_passed < 5: #and self._data['stateEstimate.z'] > 0.1:
+            # x = self._data['stateEstimate.x']
+            # y = self._data['stateEstimate.y']
             # self._cf.commander.send_position_setpoint(x, y, 0.0, 0.0)
             self._cf.commander.send_zdistance_setpoint(0.0, 0.0, 0.0, 0.0)
-            self.log_data()
+            # self.log_data()
             time.sleep(0.05)
             time_passed += 0.05
 
         print('Landed')
-        self._pylogfile.close()
-        self._logconfig.stop()
+        # self._pylogfile.close()
+        # self._logconfig.stop()
 
 
     def fly(self):
         # First we define the logging configuration as the input
-        self._cf.log.add_config(self._logconfig)
+        # self._cf.log.add_config(self._logconfig)
         # Then we add a callback to log the data into the class variables every time
-        self._logconfig.data_received_cb.add_callback(self._log_cb)
+        # self._logconfig.data_received_cb.add_callback(self._log_cb)
         # Then we start the logging
-        self._logconfig.start()
+        # self._logconfig.start()
 
         try:
             self.takeoff()
-            self.fly_through_gate(time_limit=60)
+            # self.fly_through_gate(time_limit=60)
             self.fly_forward(time_limit=5)
             print('Landing')
             self.landing()
@@ -212,17 +215,17 @@ if __name__ == '__main__':
     cflib.crtp.init_drivers()
 
     # logging configuration (x,y,z and errors)
-    lg_stab = LogConfig(name='', period_in_ms=10)
-    lg_stab.add_variable('stateEstimate.x', 'float')
-    lg_stab.add_variable('stateEstimate.y', 'float')
-    lg_stab.add_variable('stateEstimate.z', 'float')
+    # lg_stab = LogConfig(name='', period_in_ms=10)
+    # lg_stab.add_variable('stateEstimate.x', 'float')
+    # lg_stab.add_variable('stateEstimate.y', 'float')
+    # lg_stab.add_variable('stateEstimate.z', 'float')
     # lg_stab.add_variable('stateEstimate.pitch', 'float')
     # lg_stab.add_variable('stateEstimate.roll', 'float')
     # lg_stab.add_variable('stateEstimate.yaw', 'float')
-    lg_stab.add_variable('jevois.errorx', 'int16_t')
-    lg_stab.add_variable('jevois.errory', 'int16_t')
-    lg_stab.add_variable('jevois.width', 'int16_t')
-    lg_stab.add_variable('jevois.height', 'int16_t')
+    # lg_stab.add_variable('jevois.errorx', 'int16_t')
+    # lg_stab.add_variable('jevois.errory', 'int16_t')
+    # lg_stab.add_variable('jevois.width', 'int16_t')
+    # lg_stab.add_variable('jevois.height', 'int16_t')
     # lg_stab.add_variable('motion.deltaX', 'float')
     # lg_stab.add_variable('motion.deltaY', 'float')
     
@@ -238,6 +241,6 @@ if __name__ == '__main__':
     
     if cf.is_connected():
         print("Connected")
-        flight = gateIMAV(cf,lg_stab)
+        flight = gateIMAV(cf)
         flight.fly()
         
